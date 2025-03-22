0128dfe9d06fc9d65dc9614a2568aabbf9affac3
Theo Henrique <theodoro222@hotmail.com>
2025-03-22 15:14:04 -0300

    modification in background and modification margins in index

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,30 @@
 <template>
-  <section class="bg-[url('/background.png')] lg:bg-cover relative w-full lg:h-screen h-full">
+  <section class="bg-[url('/background.jpg')] bg-cover w-full lg:h-screen h-full">
+    <main class="flex flex-row w-full max-w-6xl m-auto">
+
+      <div class="flex w-full h-screen">
+        <img class="w-full mt-24" src="../assets/avatar.webp" alt="Avatar">
+      </div>
+
+      <div class="flex flex-col w-full m-auto h-screen">
+        <div class="flex flex-col flex-grow justify-center">
+          <h2 class="uppercase text-neutral-300 font-extrabold text-7xl text-end py-2">NUTRIBASE</h2>
+          <h3 class="uppercase text-neutral-300 font-medium text-2xl text-end py-3">Lorem ipsum dolor sit amet</h3>
+          <p class="text-neutral-100 font-extralight text-end">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima nam asperiores iure, magni architecto
+            labore.
+          </p>
+        </div>
+
+        <div class="flex items-center justify-end space-x-10 mb-10">
+          <h2 class="text-white">www.google.com</h2>
+          <a href="" class="bg-cyan-300 py-3 px-10 rounded-sm uppercase font-bold">Assine Já</a>
+        </div>
+
+      </div>
+    </main>
   </section>
+
 </template>
 
 <script>
@@ -9,6 +33,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
