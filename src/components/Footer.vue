fe00079c082a2fe1c553d2845a56faee1f336fe9
Theo Henrique <theodoro222@hotmail.com>
2025-03-21 23:45:55 -0300

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
-   <footer class="bg-gray-400 text-center py-4 border-t border-gray-300">
-        <p class="text-sm">&copy; 2024 Calculadora <span class="font-bold">NUTRI BASE</span>. Desenvolvido por <a class="underline uppercase font-bold" href="#">Theo Henrique.</a></p>
-      </footer>
+  <footer class="bg-gray-400 text-center py-4 border-t border-neutral-50">
+    <p class="text-sm">&copy; 2025 Calculadora <span class="font-bold">NUTRIBASE</span>. Desenvolvido por <a
+        class="uppercase font-bold" href="#">Theo Henrique.</a></p>
+  </footer>
 </template>
 
 <script>
@@ -10,6 +11,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
