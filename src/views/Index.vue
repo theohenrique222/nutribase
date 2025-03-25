1d575f74cc3257644e69a09306f8de1a00e2ad8e
Theo Henrique <theodoro222@hotmail.com>
2025-03-24 21:25:36 -0300

    correction width in avatar

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,45 +1,51 @@
 <template>
-  <!-- PRIMEIRA SEÇÃO -->
   <section class="bg-[url('/background.jpg')] bg-cover w-full lg:h-screen min-h-screen">
     <main class="flex flex-col lg:flex-row w-full max-w-6xl m-auto">
-
       <div class="flex w-full lg:h-screen justify-center">
-        <img class="w-10/12 mt-10 lg:mt-24" src="../assets/avatar.webp" alt="Avatar">
+        <img class=" mt-10 lg:mt-24" src="../assets/avatar.webp" alt="Avatar">
       </div>
-
       <div class="flex flex-col w-full m-auto lg:h-screen px-5 text-center lg:text-end">
         <div class="flex flex-col flex-grow justify-center">
           <h2 class="uppercase text-lime-400 font-extrabold text-5xl md:text-6xl lg:text-7xl py-4">NUTRIBASE</h2>
-          <h3 class="uppercase text-lime-400 font-medium text-lg md:text-xl lg:text-2xl py-3">Sua saúde, simplificada.</h3>
+          <h3 class="uppercase text-lime-400 font-medium text-lg md:text-xl lg:text-2xl py-3">Sua saúde, simplificada.
+          </h3>
           <p class="text-neutral-50 font-extralight text-base md:text-lg">
-            No NutriBase, você tem tudo o que precisa para manter sua saúde em dia sem complicação. Calcule sua ingestão diária ideal de água, descubra seu metabolismo basal e organize a distribuição exata dos seus macronutrientes. Tudo isso em uma interface rápida, intuitiva e moderna, para que você foque no que realmente importa: seu bem-estar.
+            No NutriBase, você tem tudo o que precisa para manter sua saúde em dia sem complicação. Calcule sua ingestão
+            diária ideal de água, descubra seu metabolismo basal e organize a distribuição exata dos seus
+            macronutrientes. Tudo isso em uma interface rápida, intuitiva e moderna, para que você foque no que
+            realmente importa: seu bem-estar.
           </p>
         </div>
 
-        <div class="flex flex-col lg:flex-row items-center justify-center lg:justify-end space-y-5 lg:space-y-0 lg:space-x-10 mb-10 mt-6">
+        <div
+          class="flex flex-col lg:flex-row items-center justify-center lg:justify-end space-y-5 lg:space-y-0 lg:space-x-10 mb-10 mt-6">
           <a href="/" class="text-white hover:text-lime-400">www.nutribase.com.br</a>
-          <a href="" class="bg-lime-400 text-neutral-700 hover:bg-neutral-100 py-3 px-10 rounded-sm uppercase font-bold">Assine Já</a>
+          <a href=""
+            class="bg-lime-400 text-neutral-700 hover:bg-neutral-100 py-3 px-10 rounded-sm uppercase font-bold">Assine
+            Já</a>
         </div>
       </div>
-
     </main>
   </section>
 
-  <!-- SEGUNDA SEÇÃO -->
   <section class="bg-[url('/background-2.jpg')] bg-cover w-full lg:h-screen min-h-screen">
     <main class="flex flex-col lg:flex-row w-full max-w-6xl m-auto px-5">
-
       <div class="flex flex-col w-full m-auto lg:h-screen pt-10 text-center lg:text-start">
         <div class="flex flex-col flex-grow justify-center">
           <h2 class="uppercase text-lime-400 font-extrabold text-3xl md:text-4xl py-4">Calcule, ajuste e evolua</h2>
-          <h3 class="uppercase text-lime-400 font-medium text-base md:text-lg py-3 ni">Tudo o que você precisa para cuidar do seu corpo.</h3>
+          <h3 class="uppercase text-lime-400 font-medium text-base md:text-lg py-3 ni">Tudo o que você precisa para
+            cuidar do seu corpo.</h3>
           <p class="text-neutral-100 font-extralight text-base md:text-lg w-full lg:w-10/12 m-auto lg:m-0">
-            Com o NutriBase, você não precisa complicar sua rotina. Calcule facilmente seu metabolismo basal, descubra sua necessidade diária de proteínas, carboidratos e gorduras, além de manter o controle da sua hidratação. Pensado para quem busca praticidade, seja você iniciante ou já avançado na sua jornada fitness.
+            Com o NutriBase, você não precisa complicar sua rotina. Calcule facilmente seu metabolismo basal, descubra
+            sua necessidade diária de proteínas, carboidratos e gorduras, além de manter o controle da sua hidratação.
+            Pensado para quem busca praticidade, seja você iniciante ou já avançado na sua jornada fitness.
           </p>
         </div>
 
         <div class="flex justify-center lg:justify-start mb-20 mt-8">
-          <a href="" class="bg-lime-400 text-neutral-700 py-4 px-8 rounded-3xl uppercase font-bold text-sm md:text-base">Explore nossas funcionalidades</a>
+          <a href=""
+            class="bg-lime-400 text-neutral-700 py-4 px-8 rounded-3xl uppercase font-bold text-sm md:text-base">Explore
+            nossas funcionalidades</a>
         </div>
 
       </div>
@@ -48,25 +54,30 @@
     </main>
   </section>
 
-  <!-- TERCEIRA SEÇÃO -->
   <section class="bg-[url('/background-hero.jpg')] bg-cover w-full lg:h-screen min-h-screen">
     <main class="flex flex-col lg:flex-row w-full max-w-6xl m-auto">
 
       <div class="flex w-full justify-center mt-10 lg:mt-24">
-        <img class="w-10/12" src="../assets/avatar-hero.png" alt="Avatar">
+        <img src="../assets/avatar-hero.png" alt="Avatar">
       </div>
 
       <div class="flex flex-col w-full m-auto lg:h-screen px-5 text-center lg:text-end">
         <div class="flex flex-col flex-grow justify-center">
-          <h2 class="uppercase text-lime-400 font-extrabold text-3xl md:text-4xl lg:text-5xl py-4">Dietas personalizadas, do seu jeito</h2>
-          <h3 class="uppercase text-lime-400 font-medium text-base md:text-lg py-3">Adapte sua alimentação em tempo real, de acordo com seus objetivos</h3>
+          <h2 class="uppercase text-lime-400 font-extrabold text-3xl md:text-4xl lg:text-5xl py-4">Dietas
+            personalizadas, do seu jeito</h2>
+          <h3 class="uppercase text-lime-400 font-medium text-base md:text-lg py-3">Adapte sua alimentação em tempo
+            real, de acordo com seus objetivos</h3>
           <p class="text-neutral-100 font-extralight text-base md:text-lg">
-            Não existe dieta única para todos. No NutriBase, você monta e ajusta seu plano alimentar facilmente, escolhendo entre diferentes objetivos como emagrecimento, ganho de massa ou manutenção. Tudo atualizado em tempo real, com recomendações flexíveis para acompanhar sua rotina e evolução.
+            Não existe dieta única para todos. No NutriBase, você monta e ajusta seu plano alimentar facilmente,
+            escolhendo entre diferentes objetivos como emagrecimento, ganho de massa ou manutenção. Tudo atualizado em
+            tempo real, com recomendações flexíveis para acompanhar sua rotina e evolução.
           </p>
         </div>
 
         <div class="flex justify-center lg:justify-end pb-10 mt-6">
-          <a href="" class="bg-lime-400 text-neutral-700 py-3 px-10 rounded-sm uppercase font-bold text-sm md:text-base">Monte sua dieta agora</a>
+          <a href=""
+            class="bg-lime-400 text-neutral-700 py-3 px-10 rounded-sm uppercase font-bold text-sm md:text-base">Monte
+            sua dieta agora</a>
         </div>
 
       </div>
