eeb1110cbc42de071a67075d6c89f195be2e67ed
Theo Henrique <theodoro222@hotmail.com>
2025-03-28 18:53:28 -0300

    commit correction margins hero section

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="bg-[url('/background.jpg')] lg:bg-cover bg-center bg-no-repeat w-full lg:h-screen min-h-screen">
+  <section class="bg-[url('/background.jpg')] lg:bg-cover bg-center bg-no-repeat w-full min-h-screen">
     <main class="flex flex-col-reverse lg:flex-row w-full px-2 sm:px-3 lg:max-w-6xl md:max-w-3xl max-w-xs m-auto">
-      <div class="flex w-full lg:h-screen justify-center">
-        <img class="lg:mt-24 mt-5 lg:h-auto sm:h-1/2" src="../assets/avatar.webp" alt="Avatar">
+      <div class="flex w-full">
+        <img class="lg:mt-24 mt-5 bottom-0 lg:h-auto sm:h-1/2" src="../assets/avatar.webp" alt="Avatar">
       </div>
       <div class="flex flex-col w-full m-auto lg:h-screen mt-24 lg:text-end">
         <div class="flex flex-col flex-grow justify-center">
@@ -60,20 +60,25 @@
       <div class="w-full max-w-md overflow-hidden flex justify-center">
         <img src="/avatar_basal.jpg" class="max-w-full h-auto" alt="Avatar">
       </div>
-      
+
 
       <div class="flex flex-col w-full m-auto lg:h-screen px-5 text-center lg:text-end">
         <div class="flex flex-col flex-grow justify-center">
-          <h2 class="uppercase text-lime-400 font-extrabold text-3xl md:text-4xl lg:text-3xl py-4">Descubra seu Metabolismo Basal</h2>
-          <h3 class="uppercase text-lime-400 font-medium text-base md:text-lg py-3">Entenda seu gasto calórico e otimize sua alimentação.</h3>
+          <h2 class="uppercase text-lime-400 font-extrabold text-3xl md:text-4xl lg:text-3xl py-4">Descubra seu
+            Metabolismo Basal</h2>
+          <h3 class="uppercase text-lime-400 font-medium text-base md:text-lg py-3">Entenda seu gasto calórico e otimize
+            sua alimentação.</h3>
           <p class="text-neutral-100 font-extralight text-base md:text-lg">
-            O metabolismo basal representa a quantidade de calorias que seu corpo queima em repouso para manter funções vitais como respiração e circulação sanguínea. Com o NutriBase, você calcula esse valor de forma rápida e precisa, permitindo personalizar sua alimentação e atingir seus objetivos com mais eficiência.
+            O metabolismo basal representa a quantidade de calorias que seu corpo queima em repouso para manter funções
+            vitais como respiração e circulação sanguínea. Com o NutriBase, você calcula esse valor de forma rápida e
+            precisa, permitindo personalizar sua alimentação e atingir seus objetivos com mais eficiência.
           </p>
         </div>
 
         <div class="flex justify-center lg:justify-end pb-10 mt-6">
           <a href="/basal_metabolism"
-            class="bg-lime-400 text-neutral-700 hover:bg-neutral-100 py-3 px-10 rounded-sm uppercase font-bold">Calcule agora</a>
+            class="bg-lime-400 text-neutral-700 hover:bg-neutral-100 py-3 px-10 rounded-sm uppercase font-bold">Calcule
+            agora</a>
         </div>
 
       </div>
