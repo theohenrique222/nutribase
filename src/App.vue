8571539f76d559f3df711a93faaca032f194248e
Theo Henrique <theodoro222@hotmail.com>
2025-03-29 18:37:46 -0300

    add features in new page

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
 import Calculations from "./views/pages/Calculations.vue";
+import Features from "./views/pages/Features.vue";
 
 </script>
 <template>
   <div class="flex flex-col justify-between">
     <Navbar class="fixed top-0 z-50"/>
     <router-view />
+    <Features/>
     <Calculations/>
     <Footer class="bottom-0 w-full"/>
   </div>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -27,32 +27,4 @@
       </div>
     </main>
   </section>
-
-  <section class="bg-[url('background-2.jpg')] bg-cover w-full lg:h-screen">
-    <main class="flex flex-col lg:flex-row w-full px-2 sm:px-3 lg:max-w-6xl md:max-w-3xl sm:max-w-xl max-w-xs m-auto">
-      <div class="flex flex-col w-full m-auto lg:h-screen pt-10 lg:text-start">
-        <div class="flex flex-col flex-grow justify-center">
-          <h2 class="uppercase text-lime-400 font-extrabold text-3xl md:text-4xl py-4">Calcule, ajuste e <span class="text-white">evolua</span>!</h2>
-          <h3 class="uppercase text-lime-400 font-medium text-base md:text-lg py-3 ni">Tudo o que você precisa para
-            cuidar do seu corpo.</h3>
-          <p class="text-neutral-100 font-extralight text-base md:text-lg w-full lg:w-10/12 m-auto lg:m-0">
-            Com o NutriBase, você não precisa complicar sua rotina. Calcule facilmente seu metabolismo basal, descubra
-            sua necessidade diária de proteínas, carboidratos e gorduras, além de manter o controle da sua hidratação.
-            Pensado para quem busca praticidade, seja você iniciante ou já avançado na sua jornada fitness.
-          </p>
-        </div>
-
-        <div class="flex justify-center lg:justify-start mb-20 mt-8">
-          <a href=""
-            class="bg-lime-400 text-neutral-700 py-4 px-8 rounded-3xl uppercase font-bold text-xs md:text-base">Explore
-            nossas funcionalidades</a>
-        </div>
-      </div>
-      <div class="flex w-full h-0 lg:h-screen"></div>
-    </main>
-  </section>
-
-  
-  
-  
 </template>
