69b8a8b711c9e0a99de4c2bf5cb425f8dd57f69f
Theo Henrique <theodoro222@hotmail.com>
2025-03-29 18:36:05 -0300

    add calculations in new page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
+import Calculations from "./views/pages/Calculations.vue";
 
 </script>
 <template>
   <div class="flex flex-col justify-between">
     <Navbar class="fixed top-0 z-50"/>
     <router-view />
+    <Calculations/>
     <Footer class="bottom-0 w-full"/>
   </div>
 </template>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,7 +2,7 @@
   <section class="bg-[url('background.jpg')] lg:bg-cover bg-center bg-no-repeat w-full">
     <main class="flex flex-col-reverse lg:flex-row w-full px-2 sm:px-3 lg:max-w-6xl md:max-w-3xl sm:max-w-xl max-w-xs m-auto">
       <div class="lg:flex hidden w-full">
-        <img class="lg:mt-24 mt-5 bottom-0 lg:h-auto sm:h-1/2" src="../assets/avatar.webp" alt="Avatar">
+        <img class="lg:mt-24 mt-5 bottom-0 lg:h-auto sm:h-1/2" src="/avatar.webp" alt="Avatar">
       </div>
       <div class="flex flex-col w-full m-auto lg:h-screen mt-24 lg:text-end">
         <div class="flex flex-col flex-grow justify-center">
@@ -52,56 +52,7 @@
     </main>
   </section>
 
-  <section class="bg-[url('background3.jpg')] bg-cover w-full lg:h-screen min-h-screen">
-    <main class="flex flex-col-reverse lg:flex-row w-full px-2 sm:px-3 lg:max-w-6xl md:max-w-3xl sm:max-w-xl max-w-xs m-auto lg:my-0 my-10 space-x-10">
-      <div class="flex lg:flex-col flex-col-reverse w-full m-auto lg:h-screen text-start">
-        <div class="flex flex-col flex-grow justify-center">
-          <h2 class="uppercase text-lime-400 font-extrabold text-3xl md:text-4xl lg:text-4xl py-4">
-            Otimize sua Saúde com Cálculos <span class="text-neutral-200"> Personalizados</span>
-          </h2>
-          <h3 class="uppercase text-lime-400 font-medium text-base md:text-lg py-3">
-            Descubra os números ideais para sua alimentação e bem-estar!
-          </h3>
-          <p class="text-neutral-100 font-extralight text-base md:text-lg">
-            Cada organismo tem necessidades específicas. Com nossos cálculos personalizados, você saberá exatamente quantas calorias consumir, como dividir seus macronutrientes, a quantidade ideal de água por dia e sua porcentagem de gordura corporal. Faça escolhas mais inteligentes para alcançar seus objetivos!
-          </p>
-
-          <div class="lg:flex lg:flex-row flex-col lg:py-0 py-2 text-center w-full mt-10 border-2 border-lime-500">
-
-            <div class="w-full p-2">
-              <h3 class="lg:text-xs text-base text-lime-500">Metabolismo Basal</h3>
-              <a href="" class="text-white text-xs font-extralight">Calcule agora!</a>
-            </div>
-            
-            <div class="bg-neutral-100 m-auto w-2/3 h-px lg:w-px lg:h-2/3"></div>
-
-            <div class="w-full p-2">
-              <h3 class="lg:text-xs text-base text-lime-500">Gordura corporal</h3>
-              <a href="" class="text-white text-xs font-extralight">Calcule agora!</a>
-            </div>
-
-            <div class="bg-neutral-100 m-auto w-2/3 h-px lg:w-px lg:h-2/3"></div>
-
-            <div class="w-full p-2">
-              <h3 class="lg:text-xs text-base text-lime-500">
-                Macronutrientes</h3>
-              <a href="" class="text-white text-xs font-extralight">Calcule agora!</a>
-            </div>
-
-            <div class="bg-neutral-100 m-auto w-2/3 h-px lg:w-px lg:h-2/3"></div>
-            
-            <div class="w-full p-2">
-              <h3 class="lg:text-xs text-base  text-lime-500">
-                Água diaria</h3>
-              <a href="" class="text-white text-xs font-extralight">Calcule agora!</a>
-            </div>
-            <div class="bg-neutral-100 m-auto w-2/3 h-px lg:w-0 px lg:h-2/3"></div>
-          </div>
-        </div>
-      </div>
-      <div class="w-full max-w-md overflow-hidden flex justify-center"></div>
-    </main>
-  </section>
+  
   
   
 </template>
