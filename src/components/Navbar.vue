ed2548ab425e64a5820132f019de4749be8dfbff
Theo Henrique <theodoro222@hotmail.com>
2025-03-29 18:28:45 -0300

    alteration in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,14 @@
 <template>
-  <nav class="bg-neutral-800/10 backdrop-blur-sm w-full">
-    <main class="flex justify-between lg:max-w-6xl md:max-w-3xl sm:max-w-xl max-w-xs m-auto px-2 sm:px-3 py-5 items-center">
+  <nav class=" backdrop-blur-sm w-full">
+    <main class="flex justify-between lg:max-w-6xl md:max-w-3xl max-w-xs m-auto px-2 sm:px-3 py-5 items-center">
       <ul class="flex items-center space-x-4 text-neutral-100 font-bold lg:text-3xl text-2xl">
         <li><a href="/">Nutribase</a></li>
       </ul>
 
-      <!-- Botão Hamburger -->
-      <button @click="toggleMenu" class="lg:hidden text-neutral-100 text-3xl">
+      <button @click="toggleMenu" class="lg:hidden flex text-neutral-100 text-3xl">
         <ion-icon :name="menuOpen ? 'close' : 'menu'" class="transition-all"></ion-icon>
       </button>
 
-      <!-- Menu Desktop -->
       <ul class="hidden lg:flex items-center space-x-6 text-neutral-100">
         <li v-for="item in items_menu" :key="item.id">
           <a :href="item.href" class="py-5 transition-all hover:border-b-4 border-neutral-200 text-sm">{{ item.name
@@ -27,15 +25,13 @@
       </ul>
     </main>
 
-    <!-- Menu Mobile -->
     <div v-if="menuOpen"
       class="lg:hidden absolute w-full bg-neutral-800/80 backdrop-blur-sm p-5 flex flex-col items-center space-y-4 text-neutral-100">
-      <!-- Menu Items -->
+
       <li v-for="item in items_menu" :key="item.id" class="flex border-b-2 border-b-neutral-500 w-full p-2">
         <a :href="item.href" class="block py-2 text-lg text-center w-full">{{ item.name }}</a>
       </li>
 
-      <!-- Social Icons -->
       <div class="flex space-x-4 p-2">
         <li v-for="social in social_icons" :key="social.id" class="flex">
           <a :href="social.href"
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
 import Index from '@/views/Index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'Dashboard',
-    //   component: Dashboard
-    // },
     {
       patch: '/',
       name:  'Index',
