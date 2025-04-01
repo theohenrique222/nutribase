e6fa33edb47861e0b34989f7d263b17a7d157618
Theo Henrique <theodoro222@hotmail.com>
2025-04-01 18:59:12 -0300

    alteration in content cards in modal

--- src/views/pages/Calculations.vue
+++ src/views/pages/Calculations.vue
@@ -4,20 +4,23 @@
             class="flex flex-col-reverse lg:flex-row w-full px-2 sm:px-3 lg:max-w-6xl md:max-w-3xl sm:max-w-xl max-w-xs m-auto lg:my-0 my-10 space-x-10">
             <div class="flex lg:flex-col flex-col-reverse w-full m-auto lg:h-screen text-start">
                 <div class="flex flex-col flex-grow justify-center">
-                    <h2 data-aos="fade-up" data-aos-duration="700" class="uppercase text-lime-400 font-extrabold text-3xl md:text-4xl lg:text-4xl py-4">
+                    <h2 data-aos="fade-up" data-aos-duration="700"
+                        class="uppercase text-lime-400 font-extrabold text-3xl md:text-4xl lg:text-4xl py-4">
                         Otimize sua Saúde com Cálculos <span class="text-neutral-200"> Personalizados</span>
                     </h2>
-                    <h3 data-aos="fade-up" data-aos-duration="1000" class="uppercase text-lime-400 font-medium text-base md:text-lg py-3">
+                    <h3 data-aos="fade-up" data-aos-duration="1000"
+                        class="uppercase text-lime-400 font-medium text-base md:text-lg py-3">
                         Descubra os números ideais para sua alimentação e bem-estar!
                     </h3>
-                    <p data-aos="fade-up" data-aos-duration="1000" class="text-neutral-100 font-extralight text-base md:text-lg">
+                    <p data-aos="fade-up" data-aos-duration="1000"
+                        class="text-neutral-100 font-extralight text-base md:text-lg">
                         Cada organismo tem necessidades específicas. Com nossos cálculos personalizados, você saberá
                         exatamente quantas calorias consumir, como dividir seus macronutrientes, a quantidade ideal de
                         água por dia e sua porcentagem de gordura corporal. Faça escolhas mais inteligentes para
                         alcançar seus objetivos!
                     </p>
 
-                    <div data-aos="fade-up" data-aos-duration="1000" 
+                    <div data-aos="fade-up" data-aos-duration="1000"
                         class="lg:flex lg:flex-row flex-col lg:py-0 py-2 text-center w-full mt-10 border-2 border-lime-500">
                         <template v-for="(item, index) in links" :key="item.id">
                             <div @click="openDialog(item)"
@@ -41,7 +44,7 @@
 
     <Dialog v-model:visible="visible" modal :header="selectedItem?.title || 'Detalhes'" :style="{ width: '25rem' }">
         <span class="text-surface-500 dark:text-surface-400 block mb-8">
-            Detalhes do cálculo para <strong>{{ selectedItem?.title }}</strong>
+            O que é <strong>{{ selectedItem?.title }}</strong>?
         </span>
         <p class="text-sm text-neutral-200">{{ selectedItem?.description || "Sem descrição disponível." }}</p>
         <div class="flex justify-end gap-2 mt-4">
@@ -64,21 +67,21 @@ export default {
                     title: "Metabolismo Basal",
                     button: "Calcule agora!",
                     href: "/basal_metabolism",
-                    description: "Saiba quantas calorias seu corpo precisa para funcionar em repouso."
+                    description: "O metabolismo basal (MB) é a energia mínima que seu corpo precisa para manter funções vitais em repouso."
                 },
                 {
                     id: 2,
                     title: "Gordura corporal",
                     button: "Calcule agora!",
                     href: "/bodyfat",
-                    description: "Descubra sua porcentagem de gordura corporal com precisão."
+                    description: "A gordura corporal aproximada é a porcentagem de gordura no corpo em relação ao peso total, estimada com base em peso, altura, idade e sexo."
                 },
                 {
                     id: 3,
                     title: "Macronutrientes",
                     button: "Calcule agora!",
                     href: "/macronutrients",
-                    description: "Calcule a distribuição ideal de proteínas, carboidratos e gorduras."
+                    description: "O cálculo de macronutrientes determina a quantidade ideal de proteínas, carboidratos e gorduras que você deve consumir diariamente, de acordo com seu peso, nível de atividade e objetivo."
                 },
                 {
                     id: 4,
